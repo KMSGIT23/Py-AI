82a241aabba12dcaca8957985751057de914da99
KMS <128460782+KMSGIT23@users.noreply.github.com>
2023-09-12 23:59:34 +0900

    Add files via upload

---
+++ 프로그램 예제(1).py
@@ -0,0 +1,14 @@
+#연산자 오버로딩 예제
+print(23+23)
+print("안녕"+"파이썬")
+
+#라이브러리 불러오기 예제
+import random
+
+#정수 난수 생성 예제
+a=random.randint(10, 20)
+b=random.randint(10, 20)
+
+#덧셈을 하고 결과를 출력 예제
+c=a+b
+print(a, b, c)
